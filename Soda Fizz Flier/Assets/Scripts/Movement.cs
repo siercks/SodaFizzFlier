1c0e695e2907897b7a9b0792617851a5c5cd6a6e
siercks <asiercks@gmail.com>
2022-12-02 20:17:35 -0800

    new

---
+++ Soda Fizz Flier/Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float rotationThrust = 1f;
+    Rigidbody rigidbodyCan;
+    void Start()
+    {
+        rigidbodyCan = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            RotationCalculation(rotationThrust);
+            Debug.Log("Rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            RotationCalculation(-rotationThrust);
+            Debug.Log("Rotating right");
+        }
+    }
+
+    void RotationCalculation(float rotationOnThisFrame)
+    {
+        rigidbodyCan.freezeRotation = true; // Freezing rotation so it can be manually rotated.
+        transform.Rotate(Vector3.forward * rotationOnThisFrame * Time.deltaTime);
+        rigidbodyCan.freezeRotation = false; // Unfreezing rotation so the physics system can take over.
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbodyCan.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            Debug.Log("Pressed space bar - the fizz!!!");
+        }
+    }
+}
